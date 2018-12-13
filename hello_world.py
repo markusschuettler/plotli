7052e0405b988b06268203bbcabb732cd00d9cc5
jackyOO7 <themaanonymitaet@byom.de>
2018-12-13 22:36:23 +0100

    show the rate graph for the first time

--- hello_world.py
+++ hello_world.py
@@ -3,6 +3,7 @@ from bokeh.plotting import figure
 from flask import Flask, render_template
 from multiprocessing import Process, Value
 import time
+from datetime import datetime as dt
 import numpy as np
 import requests
 
@@ -10,59 +11,66 @@ app = Flask(__name__, template_folder='.')
 
 TRANSFERWISE_KEY = 'dad99d7d8e52c2c8aaf9fda788d8acdc'
 
+filename='rates.txt'
 
 def record_loop(loop_on):
     lastrate=0
-    with open('data/test.txt', 'r') as f:
-        lastrate=float(f.readlines()[-1])
+    try:
+        with open('data/'+filename, 'r') as f:
+            lines=f.readlines()
+            if len(lines)>0:
+                lastrate=float(lines[-1].split()[1])
+                print(lastrate)
+    except:
+        pass
     while True:
         if loop_on.value == True:
             url = "https://transferwise.com/api/v1/payment/calculate?amount=1" \
                   "&sourceCurrency=CHF&targetCurrency=EUR"
             req = requests.get(url, headers={'X-Authorization-key': TRANSFERWISE_KEY})
-            rate=req.json()['transferwiseRate']
-            if rate!=lastrate:
-                with open('data/test.txt', 'a') as f:
-                    print(rate, file=f)
+            if req.status_code==requests.codes.ok:
+                rate=req.json()['transferwiseRate']
+                if rate!=lastrate:
+                    with open('data/'+filename, 'a') as f:
+                        print(dt.now().timestamp(), rate, file=f)
+                    lastrate=rate
         time.sleep(10)
 
 
 # Create the main plot
-def create_figure():
+def create_figure(data):
     # prepare some data
-    x = [0.1, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0]
-    y0 = [i ** 2 for i in x]
-    y1 = [10 ** i for i in x]
-    y2 = [10 ** (i ** 2) for i in x]
 
     # create a new plot
     p = figure(
-        tools="pan,box_zoom,reset,save", y_range=[0.001, 10 ** 11], title="magic",
-        x_axis_label='sections', y_axis_label='particles'
+        tools="pan,box_zoom,reset,save",
+        y_axis_label='EUR/CHF',x_axis_type="datetime"
     )
+    #p.sizing_mode='scale_both'
 
-    # add some renderers
-    p.line(x, x, legend="y=x")
-    p.circle(x, x, legend="y=x", fill_color="white", size=8)
-    p.line(x, y0, legend="y=x^2", line_width=3)
-    p.line(x, y1, legend="y=10^x", line_color="red")
-    p.circle(x, y1, legend="y=10^x", fill_color="red", line_color="red", size=6)
-    p.line(x, y2, legend="y=10^x^2", line_color="orange", line_dash="4 4")
+    if data is not None and len(data.shape)==2:
+        x = data[:,0].astype('i8').view('datetime64[s]')
+        y=data[:,1]
+        # add some renderers
+        p.step(x, y)
     return p
 
 
 @app.route('/')
 def hello_world():
+    data=None
+    try:
+        data=np.loadtxt('data/'+filename,delimiter=' ')
+    except Exception as e:
+        print(e)
     # Create the plot
-    plot = create_figure()
+    plot = create_figure(data)
 
     # Embed plot into HTML via Flask Render
     script, div = components(plot)
-    try:
-        with open('data/test.txt', 'r') as f:
-            text = f.readlines()[-1]
-    except Exception as e:
-        print(e)
+    if data is not None and len(data.shape)==2:
+        text = '{}CHF/EUR'.format(data[-1,1])
+    else:
         text = 'test'
     return render_template("hello_world.html", script=script, div=div, title=text)
 
