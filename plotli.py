1a43afcad600c8329905bd92be47430f2ae7b5bd
jackyOO7 <themaanonymitaet@byom.de>
2018-12-16 14:44:07 +0100

    probably break everything while trying to improve it save only new values while still plotting the graph on weekends be sort of scalable

--- plotli.py
+++ plotli.py
@@ -17,9 +17,8 @@ TRANSFERWISE_KEY = 'dad99d7d8e52c2c8aaf9fda788d8acdc'
 FILENAME = 'rates.txt'
 
 
-def record_loop(loop_on):
+def record_loop(loop_on,lasttimestamp):
     lastrate = 0
-    lasttimestamp = 0
     try:
         with open('data/' + FILENAME, 'r') as f:
             lines = f.readlines()
@@ -29,22 +28,21 @@ def record_loop(loop_on):
     except Exception as e:
         logging.error(f'Something went horribly wrong while trying to read the last rate: {e}')
 
-    while True:
-        logging.debug(f'At the beginning of record loop with loop_on={loop_on.value}')
-        if loop_on.value:
-            url = "https://transferwise.com/api/v1/payment/calculate?amount=1" \
-                  "&sourceCurrency=CHF&targetCurrency=EUR"
-            req = requests.get(url, headers={'X-Authorization-key': TRANSFERWISE_KEY})
-            logging.debug(req.status_code)
-            logging.debug(req.content)
-            if req.status_code == requests.codes.ok:
-                rate = req.json()['transferwiseRate']
-                if rate != lastrate or dt.now().timestamp() - lasttimestamp > 300:
-                    lasttimestamp = dt.now().timestamp()
-                    with open('data/' + FILENAME, 'a') as f:
-                        logging.debug(f'Writing to rates file: {lasttimestamp}, {rate}')
-                        print(lasttimestamp, rate, file=f)
-                    lastrate = rate
+    logging.debug(f'At the beginning of record loop with loop_on={loop_on.value}')
+    while loop_on.value:
+        url = "https://transferwise.com/api/v1/payment/calculate?amount=1" \
+              "&sourceCurrency=CHF&targetCurrency=EUR"
+        req = requests.get(url, headers={'X-Authorization-key': TRANSFERWISE_KEY})
+        logging.debug(req.status_code)
+        logging.debug(req.content)
+        if req.status_code == requests.codes.ok:
+            rate = req.json()['transferwiseRate']
+            if rate != lastrate:
+                lasttimestamp.value = dt.now().timestamp()
+                with open('data/' + FILENAME, 'a') as f:
+                    logging.debug(f'Writing to rates file: {lasttimestamp.value}, {rate}')
+                    print(lasttimestamp.value, rate, file=f)
+                lastrate = rate
         time.sleep(10)
 
 
@@ -55,13 +53,16 @@ def create_figure(data):
     # create a new plot
     p = figure(
         tools="pan,box_zoom,wheel_zoom,reset,save",
-        y_axis_label='EUR/CHF', x_axis_type="datetime"
+        y_axis_label='EUR/CHF', x_axis_type="datetime",
+        title='teeny tiny graph'
     )
-    # p.sizing_mode='scale_both'
-
+    p.sizing_mode='stretch_both'
     if data is not None and len(data.shape) == 2:
         x = data[:, 0].astype('i8').view('datetime64[s]')
         y = data[:, 1]
+        # always plot the last value up to the current time
+        x=np.hstack((x,dt.now()))
+        y=np.hstack((y,y[-1]))
         # add some renderers
         p.step(x, y, mode='after')
 
@@ -79,18 +80,20 @@ def create_figure(data):
 
 @app.route('/')
 def hello_world():
+    global lasttimestamp
     data = None
     try:
         data = np.loadtxt('data/' + FILENAME, delimiter=' ')
     except Exception as e:
-        logging.error(f'Something went horribly wrong while trying to read the last rate: {e}')
+        logging.error(f'Something went horribly wrong while trying to read the damned data: {e}\nas if that\'s ever gonna happen...')
     # Create the plot
     plot = create_figure(data)
 
     # Embed plot into HTML via Flask Render
     script, div = components(plot)
     if data is not None and len(data.shape) == 2:
-        text = '{}CHF/EUR'.format(data[-1, 1])
+        timetext=dt.fromtimestamp(lasttimestamp.value).strftime('%H:%M:%S')
+        text = f'{data[-1, 1]:.5f} CHF/EUR @ {timetext}'
     else:
         text = 'test'
     return render_template("plotli.html", script=script, div=div, title=text)
@@ -98,7 +101,8 @@ def hello_world():
 
 if __name__ == '__main__':
     recording_on = Value('b', True)
-    process = Process(target=record_loop, args=(recording_on,))
+    lasttimestamp=Value('d',0)
+    process = Process(target=record_loop, args=(recording_on,lasttimestamp))
     process.start()
     app.run(host='0.0.0.0', port=8080)
 
